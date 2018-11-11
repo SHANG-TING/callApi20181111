8ba4034bbb63c95d1222d9d2d7dbb6b85fdec2a1
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-11 10:51:00 +0800

    取得 https://jsonplaceholder.typicode.com/posts 並過濾掉只剩下 id、title 的陣列

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'callApi20181111';
+  constructor(private http: HttpClient) {
+    http
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .pipe(map((posts: any[]) => posts.map((x: any) => ({ id: x.id, title: x.title }))))
+      .subscribe(data => console.log(data));
+  }
 }
