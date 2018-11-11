fa6dc1a586bf912c500ef1f8cfe5c54e355830d0
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-11 11:06:10 +0800

    直接將 observable 丟到 posts , 然後在*ngFor 的部分透過 async pipe 取得api資料

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -9,13 +10,8 @@ import { map } from 'rxjs/operators';
 })
 export class AppComponent {
   title = 'callApi20181111';
-  posts: any[] = [];
+  posts: Observable<any[]> = this.http
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .pipe(map((posts: any[]) => posts.map((x: any) => ({ id: x.id, title: x.title }))));
   constructor(private http: HttpClient) {}
-
-  loadData() {
-    this.http
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .pipe(map((posts: any[]) => posts.map((x: any) => ({ id: x.id, title: x.title }))))
-      .subscribe(posts => (this.posts = posts));
-  }
 }
