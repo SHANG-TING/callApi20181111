240d6988dd04b8611523627bb320bb557aefe7b7
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-11 11:59:43 +0800

    透過route.paramMap 取得 id

--- src/app/pages/page2-detail/page2-detail.component.ts
+++ src/app/pages/page2-detail/page2-detail.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-page2-detail',
   templateUrl: './page2-detail.component.html',
-  styleUrls: ['./page2-detail.component.scss']
+  styleUrls: ['./page2-detail.component.scss'],
 })
 export class Page2DetailComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(param => console.log(param.get('id')));
   }
-
 }
