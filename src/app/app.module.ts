a6fb4ac63de7da9b1d2438160cd91b85db469f0a
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-11 11:54:50 +0800

    初步建立page1 page2 page2detail 的路由關係

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
+import { Page2DetailComponent } from './pages/page2-detail/page2-detail.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, Page1Component, Page2Component, Page2DetailComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
